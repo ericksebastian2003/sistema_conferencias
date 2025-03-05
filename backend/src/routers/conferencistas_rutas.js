2826914a597e421b62e9b0e0706c83d836c8fb6d
Erick Sebastian Caiza Quishpe <caizaquishpeericksebastian@gmail.com>
2025-03-05 12:37:26 -0500

    Actualizaciones en el front-end 23

--- backend/src/routers/conferencistas_rutas.js
+++ backend/src/routers/conferencistas_rutas.js
@@ -10,13 +10,10 @@ import {
 import verificarAutenticacion from '../middlewares/autenticacion.js';
 
 const router = Router();
-
-// Rutas de conferencistas (protegidas con autenticación)
-router.get('/conferencista', verificarAutenticacion, listarConferencistas);
+router.get('/conferencista', listarConferencistas);
 router.post('/conferencista/crear', verificarAutenticacion, crearConferencista);
 router.get('/conferencista/perfil', verificarAutenticacion, perfilConferencista);
 router.get('/conferencista/:id', verificarAutenticacion, obtenerConferencista);
 router.put('/conferencista/act/:id', verificarAutenticacion, actualizarConferencista);
 router.delete('/conferencista/deldef/:id', verificarAutenticacion, eliminarConferencistaDefinitivo);
-
 export default router;
